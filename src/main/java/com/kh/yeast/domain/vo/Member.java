a4ba64d4f1daae4cab50900bf55ec6cee4d2d2dd
wj09091 <wonjun5959@naver.com>
2025-03-31 21:00:06 +0900

    fix member

--- src/main/java/com/kh/yeast/domain/vo/Member.java
+++ src/main/java/com/kh/yeast/domain/vo/Member.java
@@ -1,6 +1,5 @@
 package com.kh.yeast.domain.vo;
 
-<<<<<<< HEAD
 import jakarta.persistence.*;
 import lombok.*;
 import java.time.LocalDate;
@@ -81,40 +80,10 @@ public class Member {
         this.address = address;
         this.positionNo = positionNo;
         this.businessNo = businessNo;
-=======
-import lombok.*;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import java.sql.Date;
-import java.sql.Timestamp;
-
-@Getter
-@ToString
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-public class Member {
-
-    private Long userNo;
-    private String businessName;
-    private String positionName;
-    private String managerName;
-    private String userName;
-    private String userId;
-    private String userPwd;
-    private String email;
-    private int salary;
-    private int bonus;
-    private String phone;
-    private String gender;
-    private String address;
-    private Timestamp enrollDate;
-    private String imageChangePath;
-    private String imageOriginPath;
-    private Date birthday;
+    }
 
     public void setUserPwd(String userPwd) {
-        this.userPwd = userPwd;
->>>>>>> develop
+        
     }
 }
