2e82d2e84b9bde443cd5cb7ddd2901ba8c8148eb
qbsb147 <qbsb147@naver.com>
2025-04-03 20:35:18 +0900

    feat: dailySales

--- src/main/reference/TimestampToDate.java
+++ src/main/reference/TimestampToDate.java
@@ -1,3 +1,5 @@
+
+//리스트 변환 예시
 breadList.forEach(bread -> {
     Timestamp createDate = bread.getCreateDate();
         if (createDate != null) {
@@ -7,3 +9,11 @@ breadList.forEach(bread -> {
             bread.setEnrollDate(null);
         }
 });
+//단일 객체 변환 예시
+Timestamp createDate = bread.getCreateDate();
+    if (createDate != null) {
+Date sqlDate = new Date(createDate.getTime());
+        bread.setEnrollDate(sqlDate);
+    } else {
+        bread.setEnrollDate(null);
+}
