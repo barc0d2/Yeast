bd43efac966cebeefe4f98e1a0a0d3fbe63bb46b
HwangInTae82 <int0802@naver.com>
2025-04-07 14:12:11 +0900

    feat: supply

--- src/main/java/com/kh/yeast/controller/branch/OrderController.java
+++ src/main/java/com/kh/yeast/controller/branch/OrderController.java
@@ -12,8 +12,17 @@ public class OrderController {
 
     private final OrderService orderService;
 
-    @GetMapping("/branch/order/orderManagement")
+    @GetMapping("/branch/order/list")
     public String orderManagement(Model model){
+        model.addAttribute("currentName", "발주신청");
+        model.addAttribute("smallCurrentName","발주신청내역");
+        return "branch/order/list";
+    }
 
+    @GetMapping("/branch/order/orderPage")
+    public String orderPage(Model model){
+        model.addAttribute("currentName", "발주신청");
+        model.addAttribute("smallCurrentName","발주신청");
+        return "branch/order/orderPage";
     }
 }
--- src/main/java/com/kh/yeast/service/branch/OrderService.java
+++ src/main/java/com/kh/yeast/service/branch/OrderService.java
@@ -1,7 +1,4 @@
 package com.kh.yeast.service.branch;
 
-import org.springframework.stereotype.Service;
-
-@Service
 public interface OrderService {
 }
